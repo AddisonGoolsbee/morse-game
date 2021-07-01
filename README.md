f5fa2ccfebb95fa1533204c86259a3a75c17aef6
Addison Goolsbee <addisongoolsbee@gmail.com>
2021-07-01 11:28:14 -0400

    finished

---
+++ README.md
@@ -0,0 +1,13 @@
+# Morse Game
+
+Tired of the endless cycle of forgetting and relearning Morse Code?
+
+No? Just me?
+
+Morse Game is a simple Python script that runs a quiz where you are given a letter/code and asked to answer with the corresponding code/letter (depending on what you select when it starts)
+
+## Running the Program
+
+Navigate to the morse-game directory and type `morse`
+
+To run the program from anywhere on your computer, type the following command: `cp morse ~/.local/bin`. If you update `morse`, you'll need to run the command `rm ~/.local/bin/morse && cp morse ~/.local/bin`
---
+++ morseGame.py
@@ -0,0 +1,116 @@
+import random
+import time
+import os
+import termios
+import sys
+import tty
+import signal
+
+morse_code = {
+    ".-": "A",
+    "-...": "B",
+    "-.-.": "C",
+    "-..": "D",
+    ".": "E",
+    "..-.": "F",
+    "--.": "G",
+    "....": "H",
+    "..": "I",
+    ".---": "J",
+    "-.-": "K",
+    ".-..": "L",
+    "--": "M",
+    "-.": "N",
+    "---": "O",
+    ".--.": "P",
+    "--.-": "Q",
+    ".-.": "R",
+    "...": "S",
+    "-": "T",
+    "..-": "U",
+    "...-": "V",
+    ".--": "W",
+    "-..-": "X",
+    "-.--": "Y",
+    "--..": "Z",
+}
+
+
+def get_char_input():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+
+    try:
+        tty.setraw(sys.stdin.fileno())
+        char = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    if char in ["\x03", "\x04"]:
+        raise KeyboardInterrupt
+    return char
+
+
+def run_game(mode: str):
+    total_time = 0.0
+    num_correct = 0
+    num_questions = 0
+    os.system("clear")
+    print("Press CTRL+C at any time to quit and view your score\n")
+    try:
+        while True:
+            if mode == "character":
+                prompt = random.sample(list(morse_code.keys()), 1)[0]
+                answer = morse_code[prompt]
+            else:
+                answer = random.sample(list(morse_code.keys()), 1)[0]
+                prompt = morse_code[answer]
+
+            start = time.time()
+            if mode == "character":
+                print(prompt)
+                guess = get_char_input()
+            else:
+                guess = input(prompt + "\n")
+            guess = guess.upper().strip()
+            end = time.time()
+
+            num_questions += 1
+            os.system("clear")
+            if guess == answer:
+                num_correct += 1
+                print(
+                    f"{guess} is correct!    Time: {end - start:0.3f}s    {num_correct}/{num_questions}\n"
+                )
+                total_time += end - start
+            elif guess == "":
+                print(f"The answer was {answer}\n")
+            else:
+                print(f"{guess} is wrong!\n")
+
+    except KeyboardInterrupt:
+        num_questions = max(num_questions, 1)
+        print(
+            f"\nAccuracy: {float(num_correct) / num_questions * 100:0.1f}%\nAverage Speed: {total_time / num_questions:0.3f}s"
+        )
+        exit()
+
+
+def main():
+    try:
+        print("Press CTRL+C to quit")
+        mode = input("Would you like to answer with the character or with the code?\n")
+        while True:
+            if (
+                mode.lower().strip().startswith("char")
+                or mode.lower().strip() == "code"
+            ):
+                mode = "code" if mode.lower().strip() == "code" else "character"
+                run_game(mode)
+            else:
+                mode = input('Invalid option, please type either "char" or "code"\n')
+    except KeyboardInterrupt:
+        print("\nYou're a bum")
+
+
+if __name__ == "__main__":
+    main()
